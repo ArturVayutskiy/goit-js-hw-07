64e3e585d0f26e9722ca2682a1768dd3e9c44087
ArturVayutskiy <vayutskiy@gmail.com>
2024-03-07 20:14:50 +0200

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector('.change-color');
+const spanElement = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanElement.textContent = randomColor;
+});
